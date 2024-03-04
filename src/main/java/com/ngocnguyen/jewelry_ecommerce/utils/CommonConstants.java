91979b050ed82ed292bbdfe231101431ffc4024a
Ngoc Nguyen <ngoc622512@gmail.com>
2024-03-04 13:50:58 +0700

    fix import

--- src/main/java/com/ngocnguyen/jewelry_ecommerce/utils/CommonConstants.java
+++ src/main/java/com/ngocnguyen/jewelry_ecommerce/utils/CommonConstants.java
@@ -2,7 +2,4 @@ package com.ngocnguyen.jewelry_ecommerce.utils;
 
 public final class CommonConstants {
     public static final String FOLDER_UPLOAD = "src/main/resources/static/upload";
-    private  CommonConstants(){
-
-    }
 }
